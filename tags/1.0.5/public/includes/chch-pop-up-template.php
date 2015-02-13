300275ef33478ef21c999f7276157f2ea89a8493
chopchoporg <chopchoporg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-13 08:20:56 +0000

    VERSION 1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/pop-up@1088767 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.5/public/includes/chch-pop-up-template.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Pop-Up CC
+ *
+ * @package   CcPopUpTemplate
+ * @author    Chop-Chop.org <shop@chop-chop.org>
+ * @license   GPL-2.0+
+ * @link      https://shop.chop-chop.org
+ * @copyright 2014 
+ */
+
+/**
+ * @package CcPopUpTemplate
+ * @author  Chop-Chop.org <shop@chop-chop.org>
+ */
+class CcPopUpTemplate { 
+
+	private $template, $template_base, $post_id = 0;
+
+	function __construct($template, $template_base, $post_id = 0) {
+		$this->plugin = CcPopUp::get_instance(); 
+		
+		$this->template = $template;
+		$this->template_base = $template_base;
+		$this->post_id = $post_id; 
+		 
+	} 
+	
+	function get_template_options(){
+		if(!$options = get_post_meta($this->post_id, '_'.$this->template.'_template_data',true)){
+			if(file_exists(CC_PU_PLUGIN_DIR . 'public/templates/'.$this->template_base.'/'.$this->template.'/defaults.php'))
+			{
+				$options = (include(CC_PU_PLUGIN_DIR . 'public/templates/'.$this->template_base.'/'.$this->template.'/defaults.php'));
+			}
+		}
+		 
+		return $options;
+	} 
+	
+	
+	function get_template(){ 
+		$template_options = $this->get_template_options();
+		$id = $this->post_id;
+		include(CC_PU_PLUGIN_DIR . 'public/templates/'.$this->template_base.'/'.$this->template.'/index.php' );  
+	}
+	
+	function build_css(){ 
+		$options = $this->get_template_options();
+		$template = $this->template_base;
+		
+		$prefix = '#modal-'.$this->post_id.' ';
+		$css = '<style>';
+		$size_options = $options['size'];
+		if($size_options['custom'])
+		{
+			$css .= $prefix.'.'.$template.'.pop-up-cc  {
+				width: '.$size_options['width'].'px;
+				height: '.$size_options['height'].'px;
+			}';
+		}
+		 
+		 
+		$background_options = $options['background'];
+		$css .= $prefix.'.'.$template.' .modal-inner { 
+			
+			background-color: '.$background_options['color'].';';
+			
+			switch($background_options['type']){ 
+				case 'image':
+					$css .= 'background-image: url('.$background_options['image'].');';
+					$css .= 'background-size: cover;';
+				break;
+				
+				case 'pattern':
+					$css .= 'background-image: url('.$background_options['pattern'].');';
+					$css .= 'background-repeat:'.$background_options['repeat'].';';
+				break; 
+			} 	
+		$css .= '}';
+		 
+		 
+		$css .= '</style>';
+	
+		echo $css;  
+	}
+	
+	function build_js()
+	{
+		$id = $this->post_id;
+		
+		$timer = get_post_meta($id, '_chch_pop_up_timer',true);
+		$timer *= 1000;
+		
+		$mobile_header = 'if($(window).width() > 1024){';
+		$mobile_footer = '}';
+		
+		if(get_post_meta($id, '_chch_pop_up_show_on_mobile',true))
+		{
+			$mobile_header = '';
+			$mobile_footer = '';	
+		}
+		
+		if(get_post_meta($id, '_chch_pop_up_show_only_on_mobile',true))
+		{
+			$mobile_header = 'if($(window).width() < 1025){'; 
+			$mobile_footer = '}';
+		}
+		
+		
+		$script = '<script type="text/javascript">';
+		$script .= 'jQuery(function($) {';
+		
+		if(get_post_meta($id, '_chch_pop_up_show_only_once',true)) {
+			$script .= 'if(!$.cookie("shown_modal_'.$id.'")){ ';
+		}
+		
+		$script .= $mobile_header;
+		
+		$script .= 'setTimeout(function(){
+				
+		
+			$("#modal-'.$id.'").show();
+						 windowPos = $(window).scrollTop();
+						 windowHeight = $(window).height();
+						 popupHeight = $( "#modal-'.$id.' .modal-inner" ).outerHeight();
+						 popupPosition = windowPos + ((windowHeight - popupHeight)/2);
+						 $( "#modal-'.$id.' .pop-up-cc").css("top",Math.abs(popupPosition)); 
+				  		
+					}, '.$timer.');  ';
+		$script .= $mobile_footer;
+		
+		if(get_post_meta($id, '_chch_pop_up_show_only_once',true)) {
+			$script .= '}';
+		}
+		$script .= '});';
+		$script .= '</script>'; 
+		
+		echo $script;		
+	}
+	
+	
+}
