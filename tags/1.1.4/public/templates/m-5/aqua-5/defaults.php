12814eb868dfc6b13b225fca3e533c2b386de05f
chopchoporg <chopchoporg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-22 19:02:07 +0000

    version 1.1.4
    git-svn-id: https://plugins.svn.wordpress.org/pop-up@1118208 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.1.4/public/templates/m-5/aqua-5/defaults.php
@@ -0,0 +1,34 @@
+<?php
+return array (
+	'size' => 
+ 	array (
+		'custom'	=> '0',
+    	'width' 	=> '784',
+		'height' 	=> 'auto',
+  	),
+	'background' => 
+ 	array (
+		'color'	=> '#fff',
+    	'type' 	=> 'no', 
+		'image' => '',
+		'pattern' => '',
+		'repeat' => 'repeat'
+  	),
+	'input' => 
+ 	array (
+		'text'	=>	'Your e-mail',
+  	),
+	'button' => 
+ 	array (
+		'text'	=>	'Submit',
+  	),
+	'contents' =>
+	array(
+		'header' 	=> 'Encourage the users <strong>to leave</strong> <br> their e-mails.',
+		'subheader' => 'Let them know what kind of content they will receive.',
+		'content' 	=> '<p> Put in some details about your campaign and list the reasons to sign up. <br>Don’t forget the final <a href="#">call to action.</a> </p>',
+		'privacy_message' 	=> 'We respect your privacy and we won\'t share your information',
+		'privacy_link' 	=> '#link',
+		'thank_you' => 'Thank you!',
+	),
+);
